0263f28037436ada06499da85bce41188f167e1f
gorag <gorbunov.alexey@gmail.com>
2023-08-02 12:09:13 +0300

    Download books from tululu.org

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-import os.path
 from pathlib import Path
-from pprint import pprint
 
 import requests
 import urllib3
 
 
-def download_book(url: str, path: Path) -> None:
-    parent_path = path.parent
-    parent_path.mkdir(parents=True, exist_ok=True)
+def download_book(url: str, directory: Path) -> None:
+    directory.mkdir(parents=True, exist_ok=True)
 
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    response = requests.get(url, verify=False)
-    response.raise_for_status()
 
-    pprint(response.text)
-
-    # with open(path, 'wb') as file:
-    #     file.write(response.content)
+    for i in range(1, 11):
+        url = f"{url}{1}"
+        response = requests.get(url, verify=False)
+        response.raise_for_status()
+        path = Path(f"{directory}/id{i}.txt")
+        with open(path, 'wb') as file:
+            file.write(response.content)
 
 
 def check_for_redirect(response):
@@ -25,6 +23,5 @@ def check_for_redirect(response):
 
 
 if __name__ == '__main__':
-    # book_url = "https://tululu.org/txt.php?id=32168"
-    book_url = "https://tululu.org/b32168/"
-    download_book(book_url, Path('books/book1.txt'))
+    book_url = "https://tululu.org/txt.php?id="
+    download_book(book_url, Path('books'))
